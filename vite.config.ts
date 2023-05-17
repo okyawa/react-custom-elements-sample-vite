3a8458517fc4aa31b4ff155c70117924f0337176
Hirokazu Okawa <h.okyawa@gmail.com>
2023-05-17 17:05:26 +0900

    add: entries内のファイルを全てinputの指定へ渡す

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glob from 'fast-glob';
+
+const entries = glob.sync('src/entries/*.ts');
+
+const input = entries.reduce((acc, entry) => {
+  const key = entry.replace('src/entries/', '').replace('.ts', '');
+  acc[key] = entry;
+  return acc;
+}, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        sample: 'src/entries/sample.tsx',
-      },
+      input,
       output: {
         entryFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name][extname]'
