8e7171fc4b85923bb1c33776c27ee49009632386
Hirokazu Okawa <h.okyawa@gmail.com>
2023-05-17 16:19:43 +0900

    add: Custom Elements内にReactを使ったサンプルを設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Props = {
+  value: string;
+  onCountChanged: (count: number) => void;
+};
+
+function App({ value, onCountChanged }: Props) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // カスタムイベントを発行
+    onCountChanged(count);
+  }, [count, onCountChanged]);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <section>
+        <h1>React Custom Elements</h1>
+        <ul>
+          <li>Custom Elements 内</li>
+        </ul>
+        <dl>
+          <dt>Value:</dt>
+          <dd>[ <strong>{value}</strong> ]</dd>
+        </dl>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <b>{count}</b>: <button type="button" onClick={handleClick}>カウントアップ</button>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,100 @@
+import ReactDOM from "react-dom";
+import { createRoot, Root } from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+/**
+ * Reactを使ったサンプルのCustom Elements
+ */
+class ReactCounter extends HTMLElement {
+  private root: Root | null;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.root = null;
+  }
+
+  /**
+   * 要素がdocumentに追加された際に実行される
+   */
+  connectedCallback() {
+    if (this.shadowRoot === null) {
+      return;
+    }
+
+    // 外部CSSファイルを読み込み
+    this.appendCSSFile('global.css')
+
+    // Web Components の属性値を取得し、Reactコンポーネントをマウント
+    const value = this.getAttribute('value') || '';
+    this.root = createRoot(this.shadowRoot);
+    this.root.render(
+      <App value={value} onCountChanged={this.handleCountChanged} />
+    );
+  }
+
+  /**
+   * 変更を監視する属性名の配列を指定
+   */
+  static get observedAttributes() {
+    return [
+      'value',
+    ];
+  }
+
+  /**
+   * observedAttributesで列挙したいずれかの属性が変更されたときに呼ばれる
+   */
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (this.root === null) {
+      return;
+    }
+    if (name === 'value' && oldValue !== newValue) {
+      // 更新された Web Components の属性値をReactコンポーネントに反映
+      this.root.render(
+        <App value={newValue} onCountChanged={this.handleCountChanged} />
+      );
+    }
+  }
+
+  /**
+   * 要素がdocumentから削除された際に実行される
+   */
+  disconnectedCallback() {
+    if (this.shadowRoot === null) {
+      return;
+    }
+    ReactDOM.unmountComponentAtNode(this.shadowRoot);
+  }
+
+  /**
+   * 外部CSSファイルを読み込み
+   */
+  private appendCSSFile(filePath: string) {
+    if (this.shadowRoot === null) {
+      return
+    }
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = filePath;
+    this.shadowRoot.appendChild(link);
+  }
+
+  /**
+   * Reactコンポーネントからカスタムイベントを実行するために渡すcallback関数
+   */
+  private handleCountChanged = (count: number) => {
+    const event = new CustomEvent('count-changed', {
+      detail: count,
+    });
+    this.dispatchEvent(event);
+  };
+}
+
+/**
+ * Custom Elementsの登録
+ */
+customElements.define("react-app", ReactCounter);
+
