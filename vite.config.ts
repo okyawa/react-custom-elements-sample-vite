b62575c5d0b4d9a9f9b9b173e3b29712eb12fa43
Hirokazu Okawa <h.okyawa@gmail.com>
2023-05-17 16:20:43 +0900

    add: Viteのbuild.rollupOptions.inputで複数のエントリポイントのビルド指定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'src/index.tsx',
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+      }
+    }
+  }
 })
